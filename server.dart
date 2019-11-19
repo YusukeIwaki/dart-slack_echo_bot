00a41ca48943da9dcead6a797f6cae7e0ccddb10
YusukeIwaki <yusuke-iwaki@example.com>
2019-11-19 23:34:10 +0900

    localhostだけをbindするかどうかと、ポート番号をコマンドライン引数で指定できるように変更

--- server.dart
+++ server.dart
@@ -1,13 +1,40 @@
 import 'dart:io';
+import 'package:args/args.dart';
 
 import './webhook.dart';
 
-Future main() async {
+class CliOptions {
+  final ArgResults parsedArgs;
+  CliOptions.parse(List<String> args)
+      : this.parsedArgs = (ArgParser()
+              ..addFlag("localhost", defaultsTo: true)
+              ..addOption("port", abbr: "p"))
+            .parse(args);
+
+  InternetAddress bindAddress() {
+    if (parsedArgs["localhost"]) {
+      return InternetAddress.loopbackIPv4;
+    } else {
+      return InternetAddress.anyIPv4;
+    }
+  }
+
+  int port() {
+    if (parsedArgs["port"] == null) {
+      return 4000;
+    }
+    return int.parse(parsedArgs["port"]);
+  }
+}
+
+Future main(List<String> args) async {
+  final options = CliOptions.parse(args);
+
   HttpServer server = await HttpServer.bind(
-    InternetAddress.anyIPv4,
-    4000,
+    options.bindAddress(),
+    options.port(),
   );
-  print("Listening on ${InternetAddress.anyIPv4.address}:4000...");
+  print("Listening on ${options.bindAddress().address}:${options.port()}...");
 
   await for (HttpRequest request in server) {
     try {
